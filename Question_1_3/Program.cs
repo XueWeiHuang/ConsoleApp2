7db99971468558ec5527e828fb5111fa3ea1e1ad
Xue Wei Huang <chauvet1302@gmail.com>
2019-07-04 08:19:56 -0400

    finished 1-4

---
+++ Question_1_3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Question_1_3
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter the word");
+            var input = Console.ReadLine().ToCharArray();
+            Console.WriteLine("enter the length");
+            var length = int.Parse(Console.ReadLine().ToString());
+            urlify(input, length);
+            Console.WriteLine(input);
+
+
+        }
+
+        public static void urlify (char[] input, int length)
+        {
+
+
+            var spaceNum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (input[i] == ' ')
+                    spaceNum++;
+            }
+
+            var newlength = length + 2 * spaceNum;
+            while(length!=0)
+            {//go from backwards because las non space c still be last c
+                if (input[length - 1] != ' ')
+                {
+                    input[length - 1 + 2 * spaceNum] = input[length - 1];
+                    length--;
+                }
+                //if the
+                else
+                {
+                    input[length - 1 + 2 * spaceNum - 2] = '%';
+                    input[length - 1 + 2 * spaceNum - 1] = '2';
+                    input[length - 1 + 2 * spaceNum] = '0';
+                    spaceNum--;
+                    length--;
+                }
+            }
+            ////cuz replace space with %20 needs 2 extra space
+            //var newLength = spaceNum*2 + length;
+            //var offset = spaceNum * 2;
+            ////input = input.ToCharArray();
+
+
+            ////now strat replace from the end of string
+            //for (int i=length-1; i>=0; i--)
+            //{
+            //    if (input[i] == ' ')
+            //    {
+            //        input[i + offset] = '0';
+            //        input[i + offset - 1] = '2';
+            //        input[i + offset - 2] = '%';
+
+            //    }
+            //    else
+            //    {
+            //        input[i + offset] = input[i];
+
+            //    }
+        
+
+        }
+    }
+}
---
+++ Question_1_4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine().ToString();
+            Console.WriteLine(IsPalindromePermutation(input));
+        }
+
+        public static bool IsPalindromePermutation(string input)
+        {
+            //is string a permutation of a palindrome
+
+            //check number of each char, all should be even for even length string, only one char count can be odd for odd length string
+            var oddCount = 0;
+            var frequencyTable = new int[26];
+            foreach (var c in input)
+            {
+                var charNumber = CharNumber(c);
+                if (charNumber!=-1)
+                {
+                    frequencyTable[charNumber]++;
+                    if (frequencyTable[charNumber] % 2 == 1)
+                        oddCount++;
+                    else
+                        oddCount--;
+
+                }
+            }
+            return oddCount <= 1;
+
+
+        }
+
+        public static int CharNumber (char c)
+        {
+            var value = char.ToLower(c) - 'a';
+            if (value >= 0 && value <= 25)
+            {
+                return value;
+            }
+            return -1;
+        }
+
+
+
+    }
+}
